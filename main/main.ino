3d4f1aee223c38016a1d81147fcd08fa935b1b23
Mitchell Aramowicz <mitchellaramowicz@icloud.com>
2025-09-23 22:50:20 +1200

    Added states for the program. Allowed the user to choose options for their plant.

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,79 @@
-void setup() {
-  // put your setup code here, to run once:
+enum State {
+  SETUP,
+  IDLE,
+  ERROR
+};
 
+enum Level {
+  LOW = 0,
+  MEDIUM,
+  HIGH
+};
+
+const char* levelNames[] = {"Low", "Medium", "High"};
+const int numLevels = 3;
+
+State currentState;
+Level moistureLevel;
+Level lightLevel;
+Level temperatureLevel;
+
+Level getLevel() {
+  char choice;
+  Level currentLevel = LOW;
+  while (1) {
+    if (currentLevel >= numLevels) {
+      currentLevel = LOW;
+    }
+
+    Serial.print(levelNames[currentLevel]);
+    Serial.println("?");
+
+    if (Serial.available()) {
+      choice = Serial.read(); // this will eventually be a button that the user presses
+      if (choice == 'y') { // 'y' means YES
+        return currentLevel;
+      } else {
+        currentLevel = (Level)(currentLevel + 1);
+      }
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  currentState = SETUP;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  switch (currentState) {
+    case SETUP:
+      Serial.println("How much soil moisture does your plant need?"); // these messages will eventually be displayed on the LCD
+      moistureLevel = getLevel();
+      Serial.print("Soil moisture set to: ");
+      Serial.println(LevelNames[moistureLevel]);
 
+      Serial.println("How much light does your plant need?");
+      lightLevel = getLevel();
+      Serial.print("Light level set to: ");
+      Serial.println(LevelNames[lightLevel]);
+
+      Serial.println("How much warmth does your plant need?");
+      temperatureLevel = getLevel();
+      Serial.print("Warmth set to: ");
+      Serial.println(LevelNames[temperatureLevel]);
+
+      currentState = IDLE;
+      break;
+
+    case IDLE:
+      // check sensors every 1 min
+      // check for error every 30 mins
+      // also check if the reset button has been pressed
+      break;
+
+    case ERROR:
+      // tell user their plant is struggling
+      break;
+  }
 }
